31d8b05253d2ab397f05c202c145be9668e07b6c
Filip-Kuta <114823329+Filip-Kuta@users.noreply.github.com>
2023-08-26 21:27:08 +0200

    Kalkulatorv1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,74 @@
-export const App = () => {
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [zarobki, setZarobki] = useState(0);
+  const [wprowadzoneDane, setWprowadzoneDane] = useState([]);
+
+  useEffect(() => {
+    // Po załadowaniu komponentu, pobierz dane z Local Storage (jeśli są dostępne)
+    const savedData = localStorage.getItem('wprowadzoneDane');
+    if (savedData) {
+      setWprowadzoneDane(JSON.parse(savedData));
+    }
+  }, []);
+
+  useEffect(() => {
+    // Po zmianie wprowadzonych danych, zapisz je w Local Storage
+    localStorage.setItem('wprowadzoneDane', JSON.stringify(wprowadzoneDane));
+  }, [wprowadzoneDane]);
+
+  const dodajDane = () => {
+    const noweDane = {
+      miesiac: new Date().toLocaleString('pl-PL', { month: 'long' }),
+      zarobki,
+      potrzeby: (zarobki * 60) / 100,
+      oszczednosci: (zarobki * 20) / 100,
+      zachcianki: (zarobki * 20) / 100,
+    };
+
+    setWprowadzoneDane([...wprowadzoneDane, noweDane]);
+    setZarobki(0); // Wyczyść pole zarobków po dodaniu danych
+
+    // Zapisz dane do Local Storage po dodaniu
+    localStorage.setItem('wprowadzoneDane', JSON.stringify([...wprowadzoneDane, noweDane]));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="container">
+      <h1>Rozdzielanie Zarobków</h1>
+      <label>Wpisz swoje zarobki miesięczne:</label>
+      <input
+        type="number"
+        value={zarobki}
+        onChange={(e) => setZarobki(parseFloat(e.target.value))}
+      />
+      <button onClick={dodajDane}>Dodaj</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Miesiąc</th>
+            <th>Zarobki</th>
+            <th>Potrzeby</th>
+            <th>Oszczędności</th>
+            <th>Zachcianki</th>
+          </tr>
+        </thead>
+        <tbody>
+          {wprowadzoneDane.map((dane, index) => (
+            <tr key={index}>
+              <td>{dane.miesiac}</td>
+              <td>{dane.zarobki} zł</td>
+              <td>{dane.potrzeby} zł</td>
+              <td>{dane.oszczednosci} zł</td>
+              <td>{dane.zachcianki} zł</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-};
+}
+
+export default App;
